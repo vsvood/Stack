19e24d9590d011e7c26ebf7336fdbca8ddc25e56
vsvood <kaspirovich.vm@phystech.edu>
2021-09-24 23:41:27 +0300

    basic version fix

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <cstdio>
-#include <climits>
 
 #include "stack.h"
 
 int main() {
   Stack stack = {};
   Stack::Ctor(&stack, 1);
-  stack.capacity = 0;
   Stack::Push(&stack, 1);
 
   int n;
   Stack::Top(&stack, &n);
-  //printf("%d", n);
+  printf("%d", n);
   Stack::Pop(&stack);
   Stack::Push(&stack, 1);
   printf("%zu %zu", stack.capacity, stack.size);
